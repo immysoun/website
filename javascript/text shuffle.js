d548e860edbd11bac001a040fb707accd2ad6f29
immy soun <immansounthon@gmail.com>
2023-02-22 22:18:58 +0000

    fixed js error on certain pages

--- javascript/text shuffle.js
+++ javascript/text shuffle.js
@@ -17,14 +17,3 @@ document.querySelector('ul').onmouseover = event => {
 		iterations += 1 / 3;
 	}, 30)
 }
-
-const blob = document.getElementById("blob");
-
-document.body.onpointermove = event => {
-	const { clientX, clientY } = event;
-
-	blob.animate({
-		left: `${clientX}px`,
-		top: `${clientY}px`
-	}, { duration: 3000, fill: "forwards" });
-}
