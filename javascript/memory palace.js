bb8654b70666724ed106668dbfbc934be23f5a26
immy soun <immansounthon@gmail.com>
2024-03-27 23:52:58 +0000

    three js

--- javascript/memory palace.js
+++ javascript/memory palace.js
@@ -1,5 +1,5 @@
 import '../css/palace.css'
-import * as THREE from 'three';
+import * as THREE from "../resources/libs/three/build/three.module";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
